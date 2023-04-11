56707221805576b8d9687d4fe5cae2e941e1c900
Andres Montes <montesalzatecaliche@gmail.com>
2023-04-11 16:49:24 -0500

    first commit

---
+++ src/products/products.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateProductInput } from './dto/inputs/create-product.input';
+import { UpdateProductInput } from './dto/inputs/update-product.input';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class ProductsService {
+
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productsRepository: Repository<Product>
+  ){}
+
+  async create(createProductInput: CreateProductInput):Promise<Product> {
+    const newItem = this.productsRepository.create(createProductInput)
+    //Se hace en dos pasos para hacer cambios antes de guadar, como por ej encriptar la contraseña
+    await this.productsRepository.save(newItem);
+    return newItem;
+  }
+
+  async  findAll():Promise<Product[]> {
+    return this.productsRepository.find();
+  }
+
+  async findOne(id: string):Promise<Product> {
+    const product = await this.productsRepository.findOneBy({id})
+
+    if (!product) throw new NotFoundException(`Product with ID ${id} not found`)
+
+    return product
+  }
+
+  update(id: number, updateProductInput: UpdateProductInput) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
